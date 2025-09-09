d88eed0c5fb6cecccea3724e43f892ebfeeb7c36
sharath453 <alsharath66@gmail.com>
2025-09-09 18:36:38 +0530

    testing

---
+++ git_testing/test.py
@@ -0,0 +1,12 @@
+import unittest
+from git_testing.test import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(1, 1), 2)
+
+if __name__ == "__main__":
+    unittest.main()
